c2df8fc9e7b3ad14616bbc57bb2b9c7294db14a2
Caio Portela <caio_fabiio@outlook.com>
2020-05-30 14:17:25 -0300

    Add solution to Hackerrank's Electronics Shop challenge

---
+++ hackerrank/electronics-shop.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import os
+import sys
+
+def getMoneySpent(keyboards, drives, b):
+    shop = [k+d if k+d <= b else -1 for d in drives for k in keyboards]
+    return max(shop)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
