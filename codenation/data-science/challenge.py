3cc55d972a3ce40b809f5658c17b1903c7e4c4ae
Caio Portela <caio_fabiio@outlook.com>
2020-06-03 00:50:36 -0300

    Add scaling to features and linear regression

--- codenation/data-science/challenge.py
+++ codenation/data-science/challenge.py
@@ -1,42 +1,45 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+LABEL = 'NU_NOTA_MT'
 
 def read(filename):
-    """
-    Reads a csv file and return its content
-    """
+    """Read a csv file and return its content."""
     return pd.read_csv(filename, index_col=0);
 
 def main():
     df_train = read('train.csv')
-    df_test = read('test.csv')
+    df_validation = read('test.csv')
 
-    LABEL = 'NU_NOTA_MT'
-
-    # Remove columns in df_train that are not in df_test
-    drop_columns = [column for column in df_train.columns if column not in df_test.columns]
+    # Remove columns in df_train that are not in df_validation
+    drop_columns = [column for column in df_train.columns if column not in df_validation.columns]
     drop_columns.remove(LABEL)  # Except for NU_NOTA_MT
 
-    for column in df_test.columns:
-        # Remove columns that have more null values over non-null on dataset
-        if df_test[column].isnull().sum() >= df_test[column].notnull().sum():
+    for column in df_validation.columns:
+        # Remove columns that have more null values over non-null on test dataset
+        if df_validation[column].isnull().sum() >= df_validation[column].notnull().sum():
             drop_columns.append(column)
 
     # Remove some other irrelevant data
-    drop_columns.extend(['SG_UF_RESIDENCIA', 'CO_PROVA_CN', 'CO_PROVA_CH',
-                         'CO_PROVA_LC', 'CO_PROVA_MT', 'IN_CEGUEIRA', 'IN_DISLEXIA',
+    drop_columns.extend(['SG_UF_RESIDENCIA', 'CO_UF_RESIDENCIA', 'CO_PROVA_CN',
+                         'CO_PROVA_CH', 'IN_SABATISTA', 'CO_PROVA_LC',
+                         'CO_PROVA_MT', 'IN_CEGUEIRA', 'IN_DISLEXIA',
                          'IN_DISCALCULIA', 'IN_IDOSO', 'IN_BAIXA_VISAO',
                          'IN_GESTANTE', 'IN_SURDEZ', 'TP_PRESENCA_CN',
-                         'TP_PRESENCA_CH', 'TP_PRESENCA_LC'])
+                         'TP_PRESENCA_CH', 'TP_PRESENCA_LC', 'NU_NOTA_COMP1',
+                         'NU_NOTA_COMP2', 'NU_NOTA_COMP3', 'NU_NOTA_COMP4',
+                         'NU_NOTA_COMP5'])
 
     drop_columns = list(set(drop_columns))
     df_train = df_train.drop(columns=drop_columns)  # Drop columns
 
     # Create a dictionary containing mapping numbers for categorical features
     question_mapping = {chr(i+64): i for i in range(1, 18)}
-    question_mapping[np.nan] = 0
 
     # Create a dictionary for the replacements
     nominal_att = ['Q001', 'Q002', 'Q006', 'Q024', 'Q025', 'Q026', 'Q047']
@@ -47,27 +50,40 @@ def main():
 
     # Replace nominal by numeric values
     df_train.replace(question_mapping, inplace=True)
-    df_test.replace(question_mapping, inplace=True)
+    df_validation.replace(question_mapping, inplace=True)
 
-    # Drop rows with missing 'NU_NOTA_MT' data
-    df_train.dropna(subset=[LABEL], inplace=True)
-
-    # Replace missing 'NU_NOTA_CH' and 'NU_NOTA_CN' with zero
+    # Replace missing data with 0
     df_train.fillna(value=0, inplace=True)
+    df_validation.fillna(value=0, inplace=True)
 
+    # Separate feature from target
+    feat_columns = list(set(df_train.columns) - set([LABEL]))
+    x_data = df_train[feat_columns].copy()
+    y_data = df_train[LABEL].copy()
+
+    # Split dataset to train and test
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2)
+
+    # Scale features
+    scale_columns = ['NU_NOTA_CH', 'NU_NOTA_CN', 'NU_NOTA_LC', 'NU_NOTA_REDACAO']
+    x_train[scale_columns] = MinMaxScaler().fit_transform(x_train[scale_columns].values)
+    y_train = pd.Series(MinMaxScaler().fit_transform(y_train.values.reshape(-1,1)))
+
+    regressor = LinearRegression().fit(x_train, y_train)
+    y_pred = regressor.predict(x_test)
+    # print(y_pred)
+    # print(y_test.head())
+
+    # df_coeff = pd.DataFrame(regressor.coef_, x_train.columns, columns=['Coefficient'])
+    # print(df_coeff.sort_values(by='Coefficient', ascending=False))
+    #
+    # df = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+    # print(df.head(25))
+
+
+    # # print(df_train.head())
     # corrmat = df_train.corr()
     # print(corrmat[LABEL].sort_values(ascending=False))
-
-    # print(df_train.groupby('CO_UF_RESIDENCIA')['CO_UF_RESIDENCIA'].count())
-
-    # for column in df_train.columns:
-    #     print('\n', df_train.groupby(column)[column].count(), df_test.groupby(column)[column].count())
-
-    # total = df_train.isnull().sum().sort_values(ascending=False)
-    # percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending=False)
-    #
-    # missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-    # print(missing_data.head())
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', 35)
